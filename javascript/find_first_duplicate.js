1ee09f937741c7bc3a9639628d21e839870f4368
jessijoke <jessijoke@yahoo.com>
2021-07-22 01:32:28 -0400

    commit tree cc1e3848c1560acea37a9c2b1d5d985c62993781

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,6 +1,62 @@
+//Read up on optimal solution
 function findFirstDuplicate(arr) {
-  // type your code here
+  let newSet = new Set();
+
+  for (var i = 0; i < arr.length; i++) {
+    if (newSet.has(arr[i])) return arr[i];
+    newSet.add(arr[i]);
+    }
+
+    return -1
 }
+
+/*
+//Attempt at a more efficient algo
+function findFirstDuplicate(arr) {
+  let newArr = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (newArr.find(element => element === arr[i])) {
+      return arr[i];
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+
+  return -1
+}*/
+
+/*
+My solution
+function findFirstDuplicate(arr) {
+  let lowestIndex = -1;
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = (i+1); j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        if (lowestIndex === -1) {
+          lowestIndex = j;
+        } else {
+          if (j < lowestIndex) lowestIndex = j;
+        } 
+      }
+    }
+  }
+  return lowestIndex === -1 ? lowestIndex : arr[lowestIndex];
+}*/
+
+/*
+Finds the first number with a duplicate value oops
+
+function findFirstDuplicate(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    newArr = arr.slice((i+1), (arr.length));
+    if (newArr.find(element => element === arr[i])) {
+      return arr[i];
+    }
+  }
+  return -1
+}
+*/
 
 if (require.main === module) {
   // add your own tests in here
